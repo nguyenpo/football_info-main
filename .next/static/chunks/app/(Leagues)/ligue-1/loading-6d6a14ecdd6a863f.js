be202401c1b237ec3e3f66236fe2863d84273b31
nguyenpo <trinhthimanhnguyencute@gmail,com>
2023-12-12 12:11:11 +0700

    up

---
+++ .next/static/chunks/app/(Leagues)/ligue-1/loading-6d6a14ecdd6a863f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[30,699,879,719,752,163,112,841,282,172,35],{3521:function(){}},function(n){n.O(0,[971,472,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
---
+++ .next/static/chunks/app/(Leagues)/primeira-liga/loading-0b6b8679e7001816.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[282,699,879,719,752,163,112,841,30,172,35],{3521:function(){}},function(n){n.O(0,[971,472,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
