c57ac78a6857c9ebaec5f87ae090d80f25b511b3
nguyenpo <trinhthimanhnguyencute@gmail,com>
2023-12-12 12:41:00 +0700

    upde

---
+++ .next/static/chunks/app/(Leagues)/copa-libertadores/page-0ecc9a554bf7e062.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[340,598,293,758,374,379,27,958,529,331,182],{6597:function(n,e,u){Promise.resolve().then(u.t.bind(u,413,23))}},function(n){n.O(0,[413,971,472,744],function(){return n(n.s=6597)}),_N_E=n.O()}]);
---
+++ .next/static/chunks/app/(Leagues)/eredivisie/loading-e075c0a774c24a01.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[163,699,752,719,879,112,30,282,841,35,172],{3521:function(){}},function(n){n.O(0,[971,472,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
